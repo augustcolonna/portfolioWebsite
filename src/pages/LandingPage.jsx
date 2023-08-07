a380c76f0ca4815c063191193e6cdc57dbe7a3b1
August Colonna <august.colonna@gmail.com>
2023-08-07 09:33:29 +0200

    landing page features added. playing with styling

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// import React from "react";
+import "../stylesheets/LandingPage.scss";
 
 function LandingPage() {
-	return <div>LandingPage</div>;
+	return (
+		<div className="nav">
+			<ul>
+				<li className="vorname">
+					<div>August</div>
+				</li>
+				<li className="projects middle">
+					<div>Projects</div>
+				</li>
+				<li className="about-me middle">
+					<div>About Me</div>
+				</li>
+				<li className="contact middle">
+					<div>Contact</div>
+				</li>
+				<li className="surname">
+					<div>Colonna</div>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default LandingPage;
