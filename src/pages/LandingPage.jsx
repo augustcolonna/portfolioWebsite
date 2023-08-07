66ee2f82517cf0a745f33c1e3d0cc92741b28caf
Gus <august.colonna@gmail.com>
2023-08-07 22:17:39 +0200

    added animations to landing page - need to make animation for strike through

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,27 +2,27 @@
 import "../stylesheets/LandingPage.scss";
 
 function LandingPage() {
-	return (
-		<div className="nav">
-			<ul>
-				<li className="vorname">
-					<div>August</div>
-				</li>
-				<li className="projects middle">
-					<div>Projects</div>
-				</li>
-				<li className="about-me middle">
-					<div>About Me</div>
-				</li>
-				<li className="contact middle">
-					<div>Contact</div>
-				</li>
-				<li className="surname">
-					<div>Colonna</div>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="nav">
+      <ul>
+        <li className="vorname animate__animated animate__slideInLeft">
+          <div>August</div>
+        </li>
+        <li className="projects animate__animated animate__flipInX">
+          <div>Projects</div>
+        </li>
+        <li className="about-me animate__animated animate__flipInX">
+          <div>About Me</div>
+        </li>
+        <li className="contact animate__animated animate__flipInX">
+          <div>Contact</div>
+        </li>
+        <li className="surname animate__animated animate__slideInRight">
+          <div>Colonna</div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default LandingPage;
