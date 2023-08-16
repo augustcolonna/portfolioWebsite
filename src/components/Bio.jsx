61c7bcf36ef2efa7f921833008b77c92aac2e302
August Colonna <august.colonna@gmail.com>
2023-08-16 10:38:29 +0200

    mapping through work histroy, solving conditional rednering problem

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,11 +1,29 @@
 import experienceData from "../utilities/work-experience.json";
+import { useState } from "react";
 
 function Bio() {
+	const [showJobDetails, setShowJobDetails] = useState(true);
+
+	function toggleShowJobDetails() {
+		setShowJobDetails(!showJobDetails);
+	}
+
 	return (
 		<div className="expereience-container">
 			{experienceData.map((experience) => {
-				return <button>{experience.jobName}</button>;
+				return (
+					<button onClick={toggleShowJobDetails} key={"_id"}>
+						{experience.jobName}
+					</button>
+				);
 			})}
+			<div>
+				{showJobDetails
+					? experienceData.map((experience) => {
+							return <p key={"_id"}>{experience.company}</p>;
+					  })
+					: ""}
+			</div>
 		</div>
 	);
 }
