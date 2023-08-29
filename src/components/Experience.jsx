38fa4c5e31ccd175b8e98a49a52ab6bb3826554c
August Colonna <august.colonna@gmail.com>
2023-08-29 12:43:45 +0200

    safety commit, I dont think anything changed though. Was not working on this project today

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,32 +3,32 @@ import { useState } from "react";
 import JobCard from "./JobCard";
 
 function Bio(props) {
-	const [job, setJob] = useState(experienceData);
-	// eslint-disable-next-line react/prop-types
-	const { toggleTheme } = props;
+  const [job, setJob] = useState(experienceData);
+  // eslint-disable-next-line react/prop-types
+  const { toggleTheme } = props;
 
-	function selectSpecificJob(jobId) {
-		const filterJobs = job.filter((job) => {
-			return job._id === jobId;
-		});
-		setJob(filterJobs);
-	}
+  function selectSpecificJob(jobId) {
+    const filterJobs = job.filter((job) => {
+      return job._id === jobId;
+    });
+    setJob(filterJobs);
+  }
 
-	return (
-		<div className="experience-container">
-			<h2 className="job-experience header">Experience</h2>
-			{experienceData.map((experience) => {
-				return (
-					<JobCard
-						key={experience._id}
-						experience={experience}
-						selectSpecificJob={selectSpecificJob}
-						toggleTheme={toggleTheme}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="experience-container">
+      <h2 className="job-experience header">Experience</h2>
+      {experienceData.map((experience) => {
+        return (
+          <JobCard
+            key={experience._id}
+            experience={experience}
+            selectSpecificJob={selectSpecificJob}
+            toggleTheme={toggleTheme}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Bio;
--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,40 +3,40 @@ import { useState } from "react";
 import "../stylesheets/main-styles.scss";
 
 function jobCard(props) {
-	const { experience, selectSpecificJob } = props;
+  const { experience, selectSpecificJob } = props;
 
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [showJobDetails, setShowJobDetails] = useState(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [showJobDetails, setShowJobDetails] = useState(false);
 
-	function toggleShowJobDetails() {
-		setShowJobDetails(!showJobDetails);
-	}
-	return (
-		<div className="expereience-information">
-			<h3 className="job-name">{experience.jobName}</h3>
-			<button
-				className={"job-button"}
-				onClick={() => {
-					selectSpecificJob(experience._id);
-					toggleShowJobDetails();
-				}}
-			>
-				{showJobDetails ? "Less" : "More"}
-			</button>
-			{showJobDetails ? (
-				<div className="job-details">
-					<ul>
-						<li>{experience.company}</li>
-						<li>{experience.location}</li>
-						<li>{experience.time}</li>
-					</ul>
-					<p>{experience.skillsGained}</p>
-				</div>
-			) : (
-				""
-			)}
-		</div>
-	);
+  function toggleShowJobDetails() {
+    setShowJobDetails(!showJobDetails);
+  }
+  return (
+    <div className="expereience-information">
+      <h3 className="job-name">{experience.jobName}</h3>
+      <button
+        className={"job-button"}
+        onClick={() => {
+          selectSpecificJob(experience._id);
+          toggleShowJobDetails();
+        }}
+      >
+        {showJobDetails ? "Less" : "More"}
+      </button>
+      {showJobDetails ? (
+        <div className="job-details">
+          <ul>
+            <li>{experience.company}</li>
+            <li>{experience.location}</li>
+            <li>{experience.time}</li>
+          </ul>
+          <p>{experience.skillsGained}</p>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default jobCard;
